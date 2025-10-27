e8e2d2f1697969e495879348d160a6e766a7ea78
AlPa83 <alina.pavlidi@laplateforme.io>
2025-10-27 11:09:28 +0100

    Exercice 2: Calculatrice java (double, puissance, modulo)

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -6,11 +6,13 @@ public class Calculatrice {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== Calculatrice ===");
-        System.out.println("Choisir une opération");
+        System.out.println("Choisissez une opération");
         System.out.println("1. Addition");
         System.out.println("2. Soustraction");
         System.out.println("3. Multiplication");
         System.out.println("4. Division");
+        System.out.println("5. Puissance");
+        System.out.println("6. Modulo");
         System.out.print("Votre choix :");
 
         int choix = scanner.nextInt();
@@ -26,16 +28,28 @@ public class Calculatrice {
         if (choix == 1) {
             resultat = num1 + num2;
             System.out.println("Résultat: " + resultat);
+
         } else if (choix == 2) {
             resultat = num1 - num2;
             System.out.println("Résultat: " + resultat);
+
         } else if (choix == 3) {
             resultat = num1 * num2;
             System.out.println("Résultat: " + resultat);
+
         } else if (choix == 4) {
             resultat = num1 / num2;
             System.out.println("Résultat: " + resultat);
+
+        } else if (choix == 5) {
+            resultat = Math.pow(num1, num2);
+            System.out.println("Résultat: " + resultat);
+
+        } else if (choix == 6) {
+            resultat = num1 % num2;
+            System.out.println("Résultat: " + resultat);
         }
+
         scanner.close();
     }
 }
